3c91797a7afb32e7c1b04a27018f0ae52203cf17
Stephan Petzl <stephan.petzl@gmail.com>
2019-06-08 15:46:26 +0200

    Enable console in production

--- app/components/Home.js
+++ app/components/Home.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import routes from '../constants/routes';
 import styles from './Home.css';
+const cv = require('opencv4nodejs');
 
 type Props = {};
 
@@ -10,6 +11,7 @@ export default class Home extends Component<Props> {
   props: Props;
 
   render() {
+    console.log("opencv4nodejs.Mat:", cv.Mat);
     return (
       <div className={styles.container} data-tid="container">
         <h2>Home</h2>
